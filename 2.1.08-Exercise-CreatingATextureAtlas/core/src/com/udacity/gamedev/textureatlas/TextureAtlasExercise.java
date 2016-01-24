86a9ceed8a7899f934bca00677d85e801130d32c
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-01-24 10:04:11 +0100

    texture atlas exercise

--- 2.1.08-Exercise-CreatingATextureAtlas/core/src/com/udacity/gamedev/textureatlas/TextureAtlasExercise.java
+++ 2.1.08-Exercise-CreatingATextureAtlas/core/src/com/udacity/gamedev/textureatlas/TextureAtlasExercise.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class TextureAtlasExercise extends ApplicationAdapter implements AssetErrorListener {
 
@@ -15,34 +17,34 @@ public class TextureAtlasExercise extends ApplicationAdapter implements AssetErr
     private static final String STANDING_RIGHT = "standing-right";
 
     // TODO: Add an AssetManager
-
+    AssetManager assetManager;
 
     SpriteBatch batch;
 
     // TODO: Add an AtlasRegion to hold the standing right sprite
-
+    TextureAtlas.AtlasRegion standingRightSprite;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
 
         // TODO: Initialize your AssetManager
-
+        assetManager = new AssetManager();
 
         // TODO: Set this as the AssetManager's error listener
-
+        assetManager.setErrorListener(this);
 
         // TODO: tell the AssetManager to load the TextureAtlas with name ATLAS
-
+        assetManager.load(ATLAS, TextureAtlas.class);
 
         // TODO: Call finishLoading() on your AssetManager
-
+        assetManager.finishLoading();
 
         // TODO: Get the TextureAtlas from the asset manager
-
+        TextureAtlas atlas = assetManager.get(ATLAS);
 
         // TODO: Populate your AtlasRegion using findRegion() on your Atlas
-
+        standingRightSprite = atlas.findRegion(STANDING_RIGHT);
     }
 
     @Override
@@ -56,7 +58,16 @@ public class TextureAtlasExercise extends ApplicationAdapter implements AssetErr
         // Hint, you need to use the somewhat horrific version of draw with signature:
         // draw (Texture texture, float x, float y, float originX, float originY, float width, float height, float scaleX,
         // float scaleY, float rotation, int srcX, int srcY, int srcWidth, int srcHeight, boolean flipX, boolean flipY)
-
+        batch.draw(standingRightSprite,
+                0,
+                0,
+                0,
+                0,
+                standingRightSprite.getRegionWidth(),
+                standingRightSprite.getRegionHeight(),
+                1,
+                1,
+                0);
         batch.end();
     }
 
@@ -69,6 +80,6 @@ public class TextureAtlasExercise extends ApplicationAdapter implements AssetErr
     public void dispose() {
         batch.dispose();
         // TODO: Dispose of the AssetManager
-
+        assetManager.dispose();
     }
 }
