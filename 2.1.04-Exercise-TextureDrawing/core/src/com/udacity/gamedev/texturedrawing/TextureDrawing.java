6f8e48d86e36d0f487e9cad8de4364393a2e83c6
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-01-24 09:25:02 +0100

    texture drawing exercise

--- 2.1.04-Exercise-TextureDrawing/core/src/com/udacity/gamedev/texturedrawing/TextureDrawing.java
+++ 2.1.04-Exercise-TextureDrawing/core/src/com/udacity/gamedev/texturedrawing/TextureDrawing.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class TextureDrawing extends ApplicationAdapter {
@@ -15,20 +17,20 @@ public class TextureDrawing extends ApplicationAdapter {
     ExtendViewport viewport;
 
     // TODO: Declare a SpriteBatch
-
+    SpriteBatch spriteBatch;
 
     // TODO: Declare a Texture for the Udacity Logo
-
+    Texture logoTexture;
 
     @Override
     public void create() {
         viewport = new ExtendViewport(WORLD_SIZE, WORLD_SIZE);
 
         // TODO: Initialize the SpriteBatch
-
+        spriteBatch = new SpriteBatch();
 
         // TODO: Load the Udacity logo Texture (look in android/assets)
-
+        logoTexture = new Texture("udacity_logo_white.png");
     }
 
     @Override
@@ -38,19 +40,20 @@ public class TextureDrawing extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // TODO: Set the SpriteBatch's projection matrix
-
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
         // TODO: Begin the sprite batch
-
+        spriteBatch.begin();
 
         // TODO: First just draw the udacity logo at LOGO_SIZE in the bottom left of the screen
-
+        spriteBatch.draw(logoTexture, (viewport.getWorldWidth() - LOGO_SIZE) / 2, (viewport.getWorldHeight() - LOGO_SIZE) / 2, LOGO_SIZE, LOGO_SIZE);
 
         // TODO: Then try centering the logo
 
 
-        // TODO: End the sprite batch
 
+        // TODO: End the sprite batch
+        spriteBatch.end();
     }
 
     @Override
